311fb8c182fc1efc710ddfcd21aa1fddf4052361
José Henrique <josehenrique.olira@gmail.com>
2026-05-11 23:18:42 -0300

    Formatando, adicionando detalhes e colocando MAIS funções

--- script.js
+++ script.js
@@ -1,73 +1,75 @@
-var nomeVelho = ""
-var cadastros = 0
-var velho = 1
-var quantiaBaixa = 0
+
+var nomeVelho = "";
+var cadastros = 0;
+var velho = 0; 
+var quantiaBaixa = 0;
+
 let nome = prompt('Digite seu nome, por favor:');
+
 while (nome == "" || nome == null) {
-    if (nome == "" || nome == null){
-        alert('Erro! Nome inválido. Por favor, digite novamente.');
-        nome = prompt('Digite seu nome');
-    } else {
-        break
-    }
+    alert('Erro! Nome inválido. Por favor, digite novamente.');
+    nome = prompt('Digite seu nome');
 }
+
 alert(`Bem-vindo ${nome}!`);
 console.log(`Usuário cadastrado. Nome: ${nome}`);
 
 alert('Iniciando cadastro de vinhos.');
 console.log('Iniciando cadastro de vinhos:');
 
-while (true){
-let opcao = prompt('Você deseja cadastrar um vinho? (SIM ou NÃO)')
-if (opcao == null){
-    alert('Fim de cadastro')
-    console.log('Usuário não cadastrou nenhum vinho.')
-    break
+while (true) {
+let opcao = prompt('Você deseja cadastrar um vinho? (SIM ou NÃO)');
+    
+if (opcao == null) {
+alert('Fim de cadastro');
+break;
 } else {
-    opcao = opcao.toLowerCase().trim()
+opcao = opcao.toLowerCase().trim();
 }
-
-if (opcao=="sim"){
-cadastros += 1
+if (opcao == "sim") {
+cadastros += 1;
 var nomeVinho = prompt('Digite o nome do vinho:');
+
+let anoFeito = parseInt(prompt('O ano que o vinho foi feito:'));
+let idadeCalculada = 2026 - anoFeito; 
+
+let estoque = parseInt(prompt(`A quantidade armazenada no estoque: `));
+
 console.log(`-----------------`);
 console.log(`Nome do vinho: ${nomeVinho}`);
-let idade = parseInt(prompt('O ano que o vinho foi feito:'));
-console.log(`Idade do vinho: ${2026 - idade}`);
-let estoque = parseInt(prompt(`A quantidade armazenada no estoque: `));
+console.log(`Idade do vinho: ${idadeCalculada} anos`);
 console.log(`Quantidade armazenada: ${estoque}`);
 
-if (idade > velho){
-    velho = 2026 - idade
-    nomeVelho = nomeVinho
-}
-if (estoque < 5){
-    console.log(`O vinho de nome: ${nomeVinho} tem um estoque baixo (${estoque}) <-------`)
-    quantiaBaixa += 1
-    var vinhoVelho = true
+      
+if (idadeCalculada > velho) {
+    velho = idadeCalculada;
+    nomeVelho = nomeVinho;
 }
 
-} else if (opcao=="nao" || opcao=="não"){
-    alert('Você escolheu não continuar')
-    if (cadastros == 0){
-    console.log('Usuário não cadastrou nenhum vinho.')
-    }
-    var vinhoVelho = false
-    break
+if (estoque < 5) {
+        console.log(`O vinho de nome: ${nomeVinho} tem um estoque baixo (${estoque}) <-------`);
+        quantiaBaixa += 1;
+}
+
+} else if (opcao == "nao" || opcao == "não") {
+    alert('Você escolheu não continuar');
+    break;
 } else {
-    alert('Opção inválida. Digite SIM ou NÃO.')
+alert('Opção inválida. Digite SIM ou NÃO.');
 }
 }
-if (cadastros > 0){
-    console.log(`Número de cadastros realizados pelo usuário ${nome}: ${cadastros}`)
-} else {
-    console.log(`Fim do programa.`)
-}
-    console.log(`O vinho mais velho é o ${nomeVelho}, com ${velho} anos.`)
+console.log('>Relatório Final<');
+
+if (cadastros > 0) {
+    console.log(`Número de cadastros realizados pelo usuário ${nome}: ${cadastros}`);
+    console.log(`O vinho mais velho é o ${nomeVelho}, com ${velho} anos.`);
     
-if (quantiaBaixa > 0){
-    alert(`${quantiaBaixa} vinhos tem uma quantidade baixa em estoque! Confira no Console.`)
-    console.log(`${quantiaBaixa} vinhos tem uma quantidade baixa em estoque. Confira acima.`)
-} if (quantiaBaixa == 0){
-    quantiaBaixa == 0
+ if (quantiaBaixa > 0) {
+    alert(`${quantiaBaixa} vinhos têm uma quantidade baixa em estoque! Confira no Console.`);
+    console.log(`${quantiaBaixa} vinhos têm uma quantidade baixa em estoque.`);
+ } else {
+console.log(`Todos os vinhos estão com estoque em dia.`);
+}
+} else {
+    console.log(`Nenhum vinho foi cadastrado. Fim do programa.`);
 }
